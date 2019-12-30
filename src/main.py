d07417d8d7d5a7d0e6a221dddfed77589175bf6a
Sergio <durden.tyler2008@gmail.com>
2019-12-30 14:14:48 +0300

    realise 1

---
+++ src/main.py
@@ -0,0 +1,12 @@
+def other_function():
+    print("other function2")
+
+
+def simple_function():
+    a = 1
+    print(a)
+
+
+if __name__ == '__main__':
+    print("test commit")
+    simple_function()
